feae43e74cc8f51ffb13dd3dc607b0d642a64388
Stefan Burgic <sburgic@smartcodehouse.com>
2021-04-04 19:29:45 +0200

    Added tests for activate and block wallet

---
+++ Core/ApplicationServices.IntegrationTests/WalletsService/WalletServiceTests.BlockWallet.cs
@@ -0,0 +1,50 @@
+﻿using Core.ApplicationServices.ApplicationDTOs;
+using Core.ApplicationServices.ApplicationExceptions;
+using Core.ApplicationServices.ApplicationServiceInterfaces;
+using Core.ApplicationServices.ApplicationServices;
+using Core.ApplicationServices.ExternalInterfaces;
+using Core.ApplicationServices.IntegrationTests.Common;
+using Core.Domain.Exceptions;
+using Core.Domain.RepositoryInterfaces;
+using Core.Infrastructure.DataAccess.Contexts;
+using Core.Infrastructure.DataAccess.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ApplicationServices.IntegrationTests.WalletsService
+{
+    public partial class WalletServiceTests
+    {
+        [TestMethod]
+        public async Task PropertiesAfterBlockWallet()
+        {
+            //Arrange
+            WalletDTO wallet = await ArrangeWallet();
+            //Act
+            WalletDTO actWallet = await _walletService.BlockWallet(wallet.Id);
+            //Assert
+            Assert.IsTrue(actWallet.Status == Domain.Entities.Enums.WalletStatus.BLOCKED);
+        }
+
+        [TestMethod]
+        public void WalletNotFoundOnBlockingWallet()
+        {
+            //Act & Assert
+            ExceptionAssert.Throws<ArgumentException>(() => _walletService.BlockWallet(0).Wait());
+        }
+
+        [TestMethod]
+        public async Task InvalidWalletStatusOnBlockingWallet()
+        {
+            //Arrange
+            WalletDTO wallet = await ArrangeWallet();
+            WalletDTO actWallet = await _walletService.BlockWallet(wallet.Id);
+            //Act & Assert
+            ExceptionAssert.Throws<WalletStatusException>(() => _walletService.BlockWallet(wallet.Id).Wait());
+        }
+    }
+}
